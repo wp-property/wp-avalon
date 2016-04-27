dbe3124da1ae0acc78eae969cd65f8000533ec58
SoSeductive <den.aka.dizel@gmail.com>
2016-04-27 19:01:29 +0300

    fixes

--- js/avalon.js
+++ js/avalon.js
@@ -136,4 +136,7 @@ jQuery(function() {
 jQuery(document).trigger('wpp_pagination_change_complete', function(e, data) {
     property_grid();
 });
+jQuery(document).bind('wpp_pagination_change_complete', function(e, data) {
+    property_grid();
+});
 
