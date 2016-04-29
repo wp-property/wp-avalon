6ca1ddf86a72b79fda895082f6de530e81a68233
SoSeductive <den.aka.dizel@gmail.com>
2016-04-29 17:33:44 +0300

    fixes

--- template-parts/content/content-archive.php
+++ template-parts/content/content-archive.php
@@ -17,7 +17,15 @@ $date_format = get_option('date_format');
         </div>
         <h1 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
         <div class="post-date"><?php the_time($date_format); ?></div>
-        <div class="post-content"><?php the_content('Read more...'); ?></div>
+        <div class="post-content">
+            <?php
+            if (!empty(get_the_content())) {
+                the_content('Read more...');
+            } elseif (!empty(category_description())) {
+                echo category_description();
+            }
+            ?>
+        </div>
         <?php the_tags('<div class="post-tags"><label>Tags: </label>', ', ', '</div>'); ?>
     </div>
 </article>
