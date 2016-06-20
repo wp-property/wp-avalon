78b0d784f0cafdfcca5bd6f0a4b780b2c693e6c1
SoSeductive <den.aka.dizel@gmail.com>
2016-06-20 11:35:06 +0300

    numeric pagination styles. Task #21

--- static/scripts/customizer/avalon-customizer.js
+++ static/scripts/customizer/avalon-customizer.js
@@ -347,6 +347,7 @@
 
   wp.customize('avalon_button_bg_color', function(value) {
     value.bind(function(newval) {
+      $('.wpp_property_overview_shortcode .properties_pagination .wpp_pagination_buttons_wrapper.pagination-numeric ul.property-overview-navigation .pages ul li a').css('background-color', newval);
       $('body main.main-content .container .frontpage-focus-widget-area .ffwa__container > div .ffwa__box .ffwa__bottom .ffwa__button a').css('background-color', newval);
       $('.widget.widget_featuredpropertieswidget .view-all .btn').css('background-color', newval);
       $('.wpp_widget.widget_featuredpropertieswidget .view-all .btn').css('background-color', newval);
@@ -396,6 +397,7 @@
   });
   wp.customize('avalon_button_border_color', function(value) {
     value.bind(function(newval) {
+      $('.wpp_property_overview_shortcode .properties_pagination .wpp_pagination_buttons_wrapper.pagination-numeric ul.property-overview-navigation .pages ul li a').css('border-color', newval);
       $('body main.main-content .container .frontpage-focus-widget-area .ffwa__container > div .ffwa__box .ffwa__bottom .ffwa__button a').css('border-color', newval);
       $('.widget.widget_featuredpropertieswidget .view-all .btn').css('border-color', newval);
       $('.wpp_widget.widget_featuredpropertieswidget .view-all .btn').css('border-color', newval);
@@ -445,6 +447,7 @@
   });
   wp.customize('avalon_button_text_color', function(value) {
     value.bind(function(newval) {
+      $('.wpp_property_overview_shortcode .properties_pagination .wpp_pagination_buttons_wrapper.pagination-numeric ul.property-overview-navigation .pages ul li a').css('color', newval);
       $('body main.main-content .container .frontpage-focus-widget-area .ffwa__container > div .ffwa__box .ffwa__bottom .ffwa__button a').css('color', newval);
       $('.widget.widget_featuredpropertieswidget .view-all .btn').css('color', newval);
       $('.wpp_widget.widget_featuredpropertieswidget .view-all .btn').css('color', newval);
@@ -494,6 +497,7 @@
   });
   wp.customize('avalon_hover_button_bg_color', function(value) {
     value.bind(function(newval) {
+      $('.wpp_property_overview_shortcode .properties_pagination .wpp_pagination_buttons_wrapper.pagination-numeric ul.property-overview-navigation .pages ul li a').css('background-color', newval);
       $('body main.main-content .container .frontpage-focus-widget-area .ffwa__container > div .ffwa__box .ffwa__bottom .ffwa__button a:hover').css('background-color', newval);
       $('.widget.widget_featuredpropertieswidget .view-all .btn:hover').css('background-color', newval);
       $('.wpp_widget.widget_featuredpropertieswidget .view-all .btn:hover').css('background-color', newval);
@@ -543,6 +547,7 @@
   });
   wp.customize('avalon_hover_button_border_color', function(value) {
     value.bind(function(newval) {
+      $('.wpp_property_overview_shortcode .properties_pagination .wpp_pagination_buttons_wrapper.pagination-numeric ul.property-overview-navigation .pages ul li a').css('border-color', newval);
       $('body main.main-content .container .frontpage-focus-widget-area .ffwa__container > div .ffwa__box .ffwa__bottom .ffwa__button a:hover').css('border-color', newval);
       $('.widget.widget_featuredpropertieswidget .view-all .btn:hover').css('border-color', newval);
       $('.wpp_widget.widget_featuredpropertieswidget .view-all .btn:hover').css('border-color', newval);
@@ -592,6 +597,7 @@
   });
   wp.customize('avalon_hover_button_text_color', function(value) {
     value.bind(function(newval) {
+      $('.wpp_property_overview_shortcode .properties_pagination .wpp_pagination_buttons_wrapper.pagination-numeric ul.property-overview-navigation .pages ul li a').css('color', newval);
       $('body main.main-content .container .frontpage-focus-widget-area .ffwa__container > div .ffwa__box .ffwa__bottom .ffwa__button a:hover').css('color', newval);
       $('.widget.widget_featuredpropertieswidget .view-all .btn:hover').css('color', newval);
       $('.wpp_widget.widget_featuredpropertieswidget .view-all .btn:hover').css('color', newval);
