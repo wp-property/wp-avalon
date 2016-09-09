9295ee929d561acd0874335d6aeee037142f44b7
Md. Alimuzzaman Alim <alimuzzamanalim@gmail.com>
2016-09-09 23:16:14 +0600

    Issue wp-property/wp-property#291

--- template-parts/content/content-property.php
+++ template-parts/content/content-property.php
@@ -79,7 +79,7 @@
         <div class="<?php wpp_css('property::property_responsive_slideshow', "property_responsive_slideshow_box"); ?>">
           <?php echo do_shortcode('[property_responsive_slideshow slider_type=carousel]'); ?>
         </div>
-      <?php elseif (!empty(get_the_post_thumbnail(get_the_ID()))) : ?>
+      <?php elseif (has_post_thumbnail(get_the_ID())) : ?>
         <div class="<?php wpp_css('property::featured_image', "wpp_featured_image"); ?>">
           <a class="fancybox_image" rel="fancybox" href="<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())); ?>">
             <?php echo get_the_post_thumbnail(get_the_ID()); ?>
