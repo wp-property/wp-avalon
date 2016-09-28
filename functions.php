fe1eb0ab10a7b70ce4464406b3d9ddf4d6aa9ff7
SoSeductive <den.aka.dizel@gmail.com>
2016-09-28 18:25:53 +0300

    New settings for Property search in welcome section #27

--- functions.php
+++ functions.php
@@ -683,7 +683,7 @@ function avalon_header_property_search()
               class="column-wrapper wpp_search_elements">
           <div class="column col-md-2 <?php echo $field_1_type; ?>_wrapper">
             <label>
-              <?php if (isset($field_1) && $field_1 !== '' && isset($wp_properties['property_stats'][$field_1])) {
+              <?php if (isset($field_1) && $field_1 !== '') {
                 echo $wp_properties['property_stats'][$field_1];
               } ?>
             </label>
